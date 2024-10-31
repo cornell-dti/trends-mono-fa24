634ccf42d4d9ab457d4660d3ca1fdb4cc54f289c
Chris Chen <144409850+cchrischen@users.noreply.github.com>
2024-10-30 21:00:26 -0400

    complete lec 6 demo (#3)

--- demos/lec6/client/src/pages/Weather.tsx
+++ demos/lec6/client/src/pages/Weather.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { WeatherResponse } from "../../../common/types";
 
 /** Implement Function to get weather from endpoint of local server */
-const getWeather = (): Promise<WeatherResponse> => {};
+const getWeather = (): Promise<WeatherResponse> => {
+    return fetch('http://localhost:8080/api/weather').then((res) => res.json());
+};
 
 const Weather = () => {
     const [{ raining }, setRaining] = useState<WeatherResponse>({
@@ -14,7 +16,10 @@ const Weather = () => {
         console.log("Loading weather...");
 
         /** Call getWeather and update state accordingly */
-        
+        getWeather().then((data) => {
+            console.log(data);
+            setRaining(data);
+        })
     }, []);
 
     return (
--- demos/lec6/server/server.ts
+++ demos/lec6/server/server.ts
@@ -10,10 +10,52 @@ const port = 8080;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("hello world!");
+});
+
+app.post("/", (req, res) => {
+    const { name } = req.body;
+    res.send(`Hi ${name}, this is a POST request.`);
+});
+
+// updating the user's name with the id
+app.put("/user/:id", (req, res) => {
+    const { newName } = req.body;
+    const id = req.params.id;
+    res.send(`Hi ${newName} at id: ${id}, this is a PUT endpoint.`);
+});
+
+// deletes data for some id up to some limit
+app.delete("/user/:id", (req, res) => {
+    const limit = req.query.limit ?? 1;
+    res.send(
+        `This is a DELETE request for at most ${limit} items for id ${req.params.id}`
+    );
+});
+
 /** Implement GET route to get the weather from:
  * "https://api.open-meteo.com/v1/forecast?latitude=40.7411&longitude=73.9897&current=precipitation&temperature_unit=fahrenheit&windspeed_unit=mph&timezone=America%2FNew_York&forecast_days=1"
  * to see if it's raining
  * Return status code 200 if successful, 500 if unsuccessful */
+
+app.get("/api/weather", async (req, res) => {
+    console.log("[GET] you hit the /api/weather endpoint")
+    try {
+        const response = await fetch(
+            "https://api.open-meteo.com/v1/forecast?latitude=40.7411&longitude=73.9897&current=precipitation&temperature_unit=fahrenheit&windspeed_unit=mph&timezone=America%2FNew_York&forecast_days=1"
+        );
+
+        const data = (await response.json()) as WeatherData;
+        const output: WeatherResponse = {
+            raining: true,
+        };
+        res.status(200).json(output);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: "Something went wrong"});
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
